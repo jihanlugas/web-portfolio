89e576b6433698d61fd93237992671be28af016b
jihanlugas <jihanlugas2@gmail.com>
2023-09-22 23:04:38 +0700

    google site verivication

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="d_Z0doP666Lm0u8e3DB4BMNCrtutRdIkup20TTWPEVM" />
+      </Head>
       <body>
         <Main />
         <NextScript />
