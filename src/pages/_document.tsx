257e17d82aa1078601d7f6ec50562544a5766027
jihanlugas <jihanlugas2@gmail.com>
2023-10-01 21:05:56 +0700

    meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="google-site-verification" content="d_Z0doP666Lm0u8e3DB4BMNCrtutRdIkup20TTWPEVM" />
+        <meta name="google-site-verification" content="k3tJdfiBldsZfpiY7RMzcbVcvH9IMILJFw037MgxaT4" />
+        <meta name="description" content={'Web portfolio Jihan Lugas'} />
+        <meta name="keywords" content="jihan lugas, jihan, programmer, portfolio, personal website, personal portfolio" />
+        <meta name="author" content="Jihan Lugas" />
       </Head>
       <body>
         <Main />
