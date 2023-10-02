5eac8de73db959a9d584e654ca0f9fc81897192f
jihanlugas <jihanlugas2@gmail.com>
2023-10-02 21:46:11 +0700

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="google-site-verification" content="k3tJdfiBldsZfpiY7RMzcbVcvH9IMILJFw037MgxaT4" />
-        <meta name="description" content={'Web portfolio Jihan Lugas ss'} />
+        <meta name="description" content={'Web portfolio Jihan Lugas'} />
         <meta name="keywords" content="jihan lugas, jihan, programmer, portfolio, personal website, personal portfolio" />
-        <meta name="author" content="Jihan Lugas ss" />
+        <meta name="author" content="Jihan Lugas" />
+        <meta property="og:image" itemProp="image" content="/favicon-192x192.png" />
+        {/* <meta property="og:site_name" content="San Roque 2014 Pollos" /> */}
+        {/* <meta property="og:title" content="San Roque 2014 Pollos" /> */}
+        {/* <meta property="og:description" content="Programa de fiestas" /> */}
+        {/* <meta property="og:type" content="website" /> */}
+        {/* <meta property="og:updated_time" content="1440432930" /> */}
       </Head>
       <body>
         <Main />
